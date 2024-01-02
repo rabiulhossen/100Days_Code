5da209fd5c2c9b9f6f88ac8358029014af1ca6df
Rabiul Hossen <rhsimon69@gmail.com>
2024-01-02 21:08:43 +0600

    dicionaris values reversed

--- dictionaries.py
+++ dictionaries.py
@@ -26,3 +26,43 @@ print(dic)
 # print all time
 for x in dic:
     print(x, dic[x])
+
+
+students_data = { 
+
+ 'Stud1': ['CS1101', 'CS2402', 'CS2001'], 
+ 'Stud2': ['CS2402','CS2001','CS1102']
+    
+
+    } 
+
+reversed_data = {
+    
+}
+
+for key in students_data:
+
+    for value in students_data[key]:
+        reversed_data[value] = key
+        
+print(reversed_data)
+        
+        
+        
+"""
+
+Inverted Output: 
+
+{ 
+
+‘CS1101’: [‘Stud1’], 
+
+‘CS2402’:['Stud1’,’Stud2’], 
+
+‘CS2001’: ['Stud1’,’Stud2’] 
+
+‘CS 1102’[‘Stud2’] 
+
+} 
+
+"""
