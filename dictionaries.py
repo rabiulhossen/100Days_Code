ad6a854e5c1f316309b58d21a97b265acbd2a707
Rabiul Hossen <rhsimon69@gmail.com>
2024-01-05 11:46:04 +0600

    dictionary problem

--- dictionaries.py
+++ dictionaries.py
@@ -19,14 +19,13 @@ print(dict())
 def histogram (s):
     d = dict()
     for c in s:
-        print(c)
-        '''
+        
         if c not in d:
             d[c]=1
         else:
             d[c]+=1
     return d
-'''
+
 print(histogram('rabiulsimon'))
 
 
